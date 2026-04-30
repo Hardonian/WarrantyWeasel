86a197003db017dbf95b7c3e9ddd903b3e77f9a4
Scott Hardie <scottrmhardie@gmail.com>
2026-04-29 22:07:52 -0400

    feat: export intel analysis utilities and registry modules from new index file

---
+++ src/lib/intel/index.ts
@@ -0,0 +1,4 @@
+export { failureScenarios, getFailureScenario, getFailureByTrigger, getAllFailureIds } from './failureRegistry'
+export { suspiciousSignals, safeSignals, getSuspiciousSignal, getSafeSignal, getMaxSuspiciousWeight } from './signalRegistry'
+export { categoryRules, edgeCases, getCategoryRule, detectCategory, getEdgeCase } from './categoryRegistry'
+export * from './constants'
